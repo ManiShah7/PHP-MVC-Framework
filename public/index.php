8244b9ae7ec991c9fc18f5673b495ae390d224ac
Mani <maani.cr7@gmail.com>
2021-04-13 22:57:27 +1000

    database

--- public/index.php
+++ public/index.php
@@ -1,13 +1,22 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use app\controllers\SiteController;
-use app\controllers\AuthController;
-
 use app\core\Application;
+use app\controllers\AuthController;
+use app\controllers\SiteController;
 
-$app = new Application(dirname(__DIR__));
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/', [new SiteController, 'home']);
 
