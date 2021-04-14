41d859ac9a2846974ec32c286db7b97cf4c43acc
Mani <maani.cr7@gmail.com>
2021-04-14 22:45:04 +1000

    migrations continue

---
+++ migrations/m0001_initial.php
@@ -0,0 +1,14 @@
+<?php
+
+class m0001_inital
+{
+    public function up()
+    {
+        echo "Applying..." . PHP_EOL;
+    }
+
+    public function down()
+    {
+        echo "Down migration." . PHP_EOL;
+    }
+}
---
+++ migrations/m0002_something.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace migrations;
+
+class m0002_something
+{
+    public function up()
+    {
+        echo "Applying..." . PHP_EOL;
+    }
+
+    public function down()
+    {
+        echo "Down migration." . PHP_EOL;
+    }
+}
