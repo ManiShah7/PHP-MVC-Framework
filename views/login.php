886abb447d654404096c15518083ad8af6b6b042
Mani <maani.cr7@gmail.com>
2021-04-19 22:24:02 +1000

    login authentication

--- views/login.php
+++ views/login.php
@@ -1,1 +1,25 @@
+<?php
+
+/** @var $model \app\models\User */
+?>
+
 <h1>Login</h1>
+<?php $form = \app\core\form\Form::begin('', 'POST'); ?>
+<div class="row">
+
+
+    <div class="col-12">
+        <?php echo $form->field($model, 'email'); ?>
+    </div>
+
+    <div class="col-md-6">
+        <?php echo $form->field($model, 'password')->passwordField(); ?>
+    </div>
+
+    <div class="col-12">
+        <button type="submit" class="btn btn-primary">
+            Submit
+        </button>
+    </div>
+</div>
+<?php \app\core\form\Form::end(); ?>
--- views/register.php
+++ views/register.php
@@ -1,3 +1,8 @@
+<?php
+
+/** @var $model \app\models\User */
+?>
+
 <h1>Register</h1>
 <?php $form = \app\core\form\Form::begin('', 'POST'); ?>
 <div class="row">
